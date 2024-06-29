c4893f7a65e89c1973b3b100f9bb712af7b6b635
Deep-Thakkar-1910 <dthakkar477@gmail.com>
2024-06-29 11:38:55 +0530

    feat/added end to end auth

---
+++ app/(Auth)/signout/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { toast } from "@/components/ui/use-toast";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+/* 
+this page will never be displayed it is just a workaround to signout the user
+using the signout function from next-auth
+*/
+const SignOutPage = () => {
+  const router = useRouter();
+  useEffect(() => {
+    signOut({ redirect: false });
+    toast({
+      title: "Success",
+      description: "You have been signed out",
+    });
+    router.push("/");
+  }, [router]);
+  return null;
+};
+
+export default SignOutPage;

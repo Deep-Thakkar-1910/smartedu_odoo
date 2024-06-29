585808d27e02515fe300a405dc2c526dbe61863a
ayushvyas-1 <ayushkv360@gmail.com>
2024-06-29 12:28:35 +0530

    add avatar

--- app/(platform)/(dashboard)/_components/Navbar.tsx
+++ app/(platform)/(dashboard)/_components/Navbar.tsx
@@ -1,9 +1,22 @@
+"use client";
 import React from 'react'
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
 import { Card, CardHeader, CardContent } from "@/components/ui/card"
+import { useSession } from 'next-auth/react'
+import { UserCircle2 } from 'lucide-react'
+
 const Navbar = () => {
+
+    const {data:session}= useSession();
+    const name = session  && session?.user?.name?.split(" ");
+    const AvatarName =
+      name && name.length > 1
+        ? name[0][0]?.toUpperCase() + name[1][0]?.toUpperCase()
+        : name && name[0][0]?.toUpperCase();
+  
+
   return (
     <>
     <header className="sticky top-0 z-40 border-b bg-background px-4 py-3 sm:px-6">
@@ -33,10 +46,12 @@ const Navbar = () => {
             <Button variant="ghost" size="icon" className="sm:hidden">
               <MenuIcon className="h-6 w-6" />
             </Button>
-            <Avatar className="h-8 w-8 border">
-              <AvatarImage src="/placeholder-user.jpg" />
-              <AvatarFallback>AC</AvatarFallback>
-            </Avatar>
+            <Avatar>
+          <AvatarImage src={session?.user?.image!} />
+          <AvatarFallback>
+            {AvatarName ?? <UserCircle2 className="size-full" />}
+          </AvatarFallback>
+        </Avatar>
           </div>
         </div>
       </header>

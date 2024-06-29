1f908da78afec824b122781caa097b20806e3900
ayushvyas-1 <ayushkv360@gmail.com>
2024-06-29 12:03:12 +0530

    Add Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
+import Navbar from "./(platform)/(dashboard)/_components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar/>
         {children}
+
         <Toaster />
       </body>
     </html>
